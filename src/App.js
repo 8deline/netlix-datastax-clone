fb9d2cf9827b4fe107232c1e855f7e56524ef644
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2021-07-07 14:05:03 +0800

    added a genre section component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-function App() {
-  return <> </>;
-}
+import fetch from "node-fetch";
+import { useState, useEffect } from "react";
+import Section from "./components/section";
 
+function App() {
+  const genres = async () => {
+    const response = await fetch(".netlify/functions/getGenres");
+    const responseBody = await response.json();
+    console.log(response);
+    console.log(responseBody);
+    console.log("test");
+  };
+
+  useEffect(() => {
+    genres();
+  });
+  console.log("test");
+  //genres();
+  return (
+    <>
+      <Section />
+    </>
+  );
+}
+// const response = fetch("/.netlify/functions/getGenres");
+// const responseBody = response.JSON();
+// console.log(responseBody);
+// console.log("test");
 export default App;
---
+++ src/components/section.jsx
@@ -0,0 +1,5 @@
+function Section(props) {
+  return <p>{props.genre}</p>;
+}
+
+export default Section;
